244832d8b7bfe57db1ea6527ef58e20bcf0270f3
Deekshigowda08 <149522963+Deekshigowda08@users.noreply.github.com>
2025-03-27 00:18:31 +0530

    final

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import { Toaster } from "@/components/ui/sonner";
@@ -6,37 +6,39 @@ import LoaderProvider from "@/components/ui/Custom/ToastLoader";
 import { SessionProvider } from "next-auth/react";
 import { XpProvider } from "@/contexts/xp";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
+const robotoSans = Roboto({
+  variable: "--font-roboto-sans",
+  subsets: ["latin"],
+  weight: ["400", "700"], // Specify font weights if needed
 });
 
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
+  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata = {
-    title: "Yukthi",
-    description: "Ultimate platform to master any concept",
+  title: "Yukthi",
+  description: "Ultimate platform to master any concept",
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                <SessionProvider>
-                    <XpProvider> 
-                        <LoaderProvider>
-                            <Navbar />
-                            <main className="pt-16 relative">{children}</main>
-                            <Toaster richColors />
-                        </LoaderProvider>
-                    </XpProvider>
-                </SessionProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body
+        className={`${robotoSans.variable} ${robotoMono.variable} antialiased`}
+      >
+        <SessionProvider>
+          <XpProvider>
+            <LoaderProvider>
+              <Navbar />
+              <main className="pt-16 relative">{children}</main>
+              <Toaster richColors />
+            </LoaderProvider>
+          </XpProvider>
+        </SessionProvider>
+      </body>
+    </html>
+  );
 }
