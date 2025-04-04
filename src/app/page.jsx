4f396313be31d5069a525d2c30f61c74e7019c21
akashr206 <akashr6514@gmail.com>
2025-04-04 20:15:24 +0530

    change landinng page to server component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,28 +1,11 @@
-"use client";
+import { auth } from "./auth";
 import Landing from "@/components/Landing/Landing";
-import { Loader2 } from "lucide-react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
-const Page = () => {
-    const { data: session, status } = useSession();
-    const router = useRouter();
-
-    useEffect(() => {
-        if (session?.user) {
-            router.replace("/roadmap");
-        }
-    }, [session, router]);
-
-    if (status === "loading" || status === "authenticated") {
-        return (
-            <div className="w-screen h-[calc(100vh-64px)] bg-background flex items-center justify-center">
-                <Loader2 className="animate-spin ease-in-out"></Loader2>
-            </div>
-        );
+import { redirect } from "next/navigation";
+const Page = async () => {
+    const session = await auth();
+    if (session?.user) {
+        redirect("/roadmap");
     }
-
     return (
         <div className="relative">
             <Landing />
